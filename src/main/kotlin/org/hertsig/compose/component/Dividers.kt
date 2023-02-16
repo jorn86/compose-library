05c8f3645025b236a53e2eb33ca565d48af7d0df
Jorn Hertsig <jorn86@gmail.com>
2023-02-16 19:21:07 +0100

    Add components from commander

--- src/main/kotlin/org/hertsig/compose/component/Dividers.kt
+++ src/main/kotlin/org/hertsig/compose/component/Dividers.kt
@@ -5,17 +5,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun HorizontalDivider(thickness: Dp = 1.dp) {
+private fun defaultColor() = MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+
+@Composable
+fun HorizontalDivider(color: Color = defaultColor(), thickness: Dp = 1.dp) {
     Divider(Modifier.fillMaxWidth().height(thickness))
 }
 
 @Composable
-fun VerticalDivider(thickness: Dp = 1.dp) {
+fun VerticalDivider(color: Color = defaultColor(), thickness: Dp = 1.dp) {
     Divider(Modifier.fillMaxHeight().width(thickness))
 }
